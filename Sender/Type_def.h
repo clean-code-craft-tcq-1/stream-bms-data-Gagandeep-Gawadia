e24d44de8789d77684130b66f1ac0e447c9bd43f
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-05-29 20:32:31 +0530

    Update Type_def.h

--- Sender/Type_def.h
+++ Sender/Type_def.h
@@ -1,5 +1,5 @@
 #pragma once
-#define UNITTEST 0
+#define UNITTEST 1
 #define NotANum 101 // send as data to signify erroneous data and read was not successful
 #define NoOfParamterSet 15
 #define NoOfPar 2 
