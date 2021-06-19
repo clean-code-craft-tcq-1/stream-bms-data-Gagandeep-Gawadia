b694b09202f2487bbca566dc81a07e8fa81b0994
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-06-19 23:47:03 +0530

    Update ReceiveData.java

--- receiver/ReceiveData.java
+++ receiver/ReceiveData.java
@@ -8,13 +8,15 @@ public class ReceiveData {
 	public static void main(String[] args) {
 		String incomingData ;
 		int incomingDataLimit = 15, incomingDataCounter = 0;
-		Console consoleReader = System.console();
 		int[] singleRecord = new int[2];
 		List<Integer> temperature = new ArrayList<Integer>();
 		List<Integer> soc = new ArrayList<Integer>();
 		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		while((incomingData = reader.readLine()) != null) {
+			Scanner in = new Scanner(System.in);
+			incomingData = in.nextLine();
+		while(incomingDataCounter <= incomingDataLimit) {
+			incomingDataCounter++;
+			incomingData = in.nextLine();
 			if(incomingDataCounter == 0) {
 				continue;
 			}
@@ -27,8 +29,7 @@ public class ReceiveData {
 				System.exit(1);
 			}
 			temperature.add(singleRecord[0]); 
-			soc.add(singleRecord[1]);
-			incomingDataCounter++;
+			soc.add(singleRecord[1]);			
 		}
 		printMaxAndMinValuesInParameter("Temperature", getMaxValueInParameter(temperature),getMinValueInParameter(temperature));
 		printMaxAndMinValuesInParameter("SOC", getMaxValueInParameter(soc),getMinValueInParameter(soc));
@@ -45,7 +46,6 @@ public class ReceiveData {
 			return null;
 		int[] singleRecord = new int[2];
 		String[] consoleData = readData.split(";");
-		System.out.println(consoleData.length);
 		if(consoleData.length < 2) 
 			return null;
 		singleRecord[0] = Integer.parseInt(consoleData[0]);
@@ -62,17 +62,15 @@ public class ReceiveData {
 	}
 	
 	public static void printMaxAndMinValuesInParameter(String paramName, int maxValue, int minValue) {
-		System.out.println(paramName + "Maximum Value : "+ maxValue +"\t"+ "Minimum Value : "+ minValue);
+		System.out.println(paramName + " Maximum Value : "+ maxValue +"\t"+ "Minimum Value : "+ minValue);
 	}
 	
 	public static int getSimpleMovingAverageOfLastFiveParamValues(List<Integer> paramList) {
-		System.out.println("from "+ (paramList.size() - 5) + "to "+ (paramList.size() - 1));
 		List<Integer> lastFiveValues = paramList.subList(paramList.size() - 5, paramList.size());
 		int sum = 0;
 		try {
 			for(Integer paramValue: lastFiveValues) {
 				sum = sum + paramValue;
-				System.out.println(paramValue );
 			}
 		}
 		catch(Exception e) {
@@ -82,6 +80,6 @@ public class ReceiveData {
 	}
 	
 	public static void printSimpleMovingAverageForParams(String paramName, int averageValue) {
-		System.out.println("Simple moving average of "+ paramName + "is " + averageValue +"\n");
+		System.out.println("Simple moving average of "+ paramName + " is " + averageValue +"\n");
 	}
 }
