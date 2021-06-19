5694fdf4fad67ac3fdd42066f2cbac8847ea7097
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-06-19 20:01:24 +0530

    Update TestReceiver.java

--- Receiver/test/TestReceiver.java
+++ Receiver/test/TestReceiver.java
@@ -1,7 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
