584a2f1c2de33b88d415e4e459267424c19e361e
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-06-19 22:18:03 +0530

    Update TestReceiver.java

--- receiver/test/TestReceiver.java
+++ receiver/test/TestReceiver.java
@@ -1,50 +1,50 @@
 package test;
 
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.AssertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
+//import BeforeEach 
+import org.junit.jupiter.api.*;
 
 import main.ReceiveData;
-
 public class TestReceiver {
-	List<Integer> paramList; 
-	ReceiveData receiveData;
-	
-	@Before
-	public void executedBeforeEach() {
-		paramList = new ArrayList<Integer>(Arrays.asList(50,80,30,20,60));
-		receiveData = new ReceiveData();
-	}
-	
-	@Test
-	public void givenParamList_whenContainsValue_thenReturnMaxValue() {
-		assertEquals(80, receiveData.getMaxValueInParameter(paramList));
-	}
-	
-	@Test
-	public void givenParamList_whenContainsValue_thenReturnMinValue() {
-		assertEquals(20, receiveData.getMinValueInParameter(paramList));
-	}	
-	
-	@Test
-	public void givenParamList_whenContainsFiveValues_thenReturnSimpleMovingAverage() {
-		assertEquals(48, receiveData.getSimpleMovingAverageOfLastFiveParamValues(paramList));
-	}
-	
-	@Test
-	public void givenSingleRecord_whenContainsNull_thenReturnNull() {
-		assertNull(receiveData.dataParser("null;null;"));
-	}
 
-	@Test
-	public void givenReadings_whenEmpty_thenReturnNull() {
-		assertNull(receiveData.dataParser(";;"));
-	}
-	
+  List<Integer> paramList;
+  ReceiveData receiveData;
+
+  @BeforeEach
+  public void executedBeforeEach() {
+    this.paramList = new ArrayList<Integer>(Arrays.asList(50, 80, 30, 20, 60));
+    this.receiveData = new ReceiveData();
+  }
+
+  @Test
+  public void givenParamList_whenContainsValue_thenReturnMaxValue() {
+    assertEquals(80, ReceiveData.getMaxValueInParameter(this.paramList));
+  }
+
+  @Test
+  public void givenParamList_whenContainsValue_thenReturnMinValue() {
+    assertEquals(20, ReceiveData.getMinValueInParameter(this.paramList));
+  }
+
+  @Test
+  public void givenParamList_whenContainsFiveValues_thenReturnSimpleMovingAverage() {
+    assertEquals(48, ReceiveData.getSimpleMovingAverageOfLastFiveParamValues(this.paramList));
+  }
+
+  @Test
+  public void givenSingleRecord_whenContainsNull_thenReturnNull() {
+    assertNull(ReceiveData.dataParser("null;null;"));
+  }
+
+  @Test
+  public void givenReadings_whenEmpty_thenReturnNull() {
+    assertNull(ReceiveData.dataParser(";;"));
+  }
+
 }
