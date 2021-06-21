843a7a852fe1ccce3b1c82035ef60b4661bb887f
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-06-21 11:30:20 +0530

    Update ReceiveData.java

--- receiver/ReceiveData.java
+++ receiver/ReceiveData.java
@@ -13,17 +13,41 @@ public class ReceiveData {
 		List<Integer> soc = new ArrayList<Integer>();
 		try {
 			Scanner in = new Scanner(System.in);
-		while(true) {
-				incomingData = in.nextLine();				
-				if(incomingData.equalsIgnoreCase("Temperature;Soc;")) {
-					break;
-				}
+			
+			//to skip unnecessary statements printed in console by the sender 
+			while(true) {
+					incomingData = in.nextLine();				
+					if(incomingData.equalsIgnoreCase("Temperature;Soc;")) {
+						break;
+					}
 						}
+			readSenderDataFromConsole(in, incomingData, incomingDataLimit,incomingDataCounter, singleRecord, temperature, soc);
+			printMaxAndMinValuesInParameter("Temperature", getMaxValueInParameter(temperature),getMinValueInParameter(temperature));
+			printMaxAndMinValuesInParameter("SOC", getMaxValueInParameter(soc),getMinValueInParameter(soc));
+			printSimpleMovingAverageForParams("Temperature",getSimpleMovingAverageOfLastFiveParamValues(temperature));
+			printSimpleMovingAverageForParams("SOC",getSimpleMovingAverageOfLastFiveParamValues(soc));
+			}
+			catch(Exception e) {
+				System.err.println(e);
+			}
+	}
+	
+	/**
+	 * reads the temperature and soc values from sender console
+	 * @param in - Scans the console to read data
+	 * @param incomingData - stores the data read from console
+	 * @param incomingDataLimit - number of data to be read from console
+	 * @param incomingDataCounter - initial value of the counter
+	 * @param singleRecord - a record of temperature & soc value
+	 * @param temperature - stores temperature value read from console
+	 * @param soc - stores soc value read from console
+	 */
+	public static void readSenderDataFromConsole(Scanner in, String incomingData, int incomingDataLimit, int incomingDataCounter, int[] singleRecord, List<Integer> temperature, List<Integer> soc) {
 		while(true) {			
 			incomingData = in.nextLine();
 			if(incomingDataCounter == incomingDataLimit)
 				break;
-			singleRecord = dataParser(incomingData);
+			singleRecord = dataParser(incomingData); 
 			if(singleRecord == null) {
 				System.out.println("Not a valid data" );
 				System.exit(1);
@@ -32,17 +56,13 @@ public class ReceiveData {
 			soc.add(singleRecord[1]);
 			incomingDataCounter++;
 		}
-			
-		printMaxAndMinValuesInParameter("Temperature", getMaxValueInParameter(temperature),getMinValueInParameter(temperature));
-		printMaxAndMinValuesInParameter("SOC", getMaxValueInParameter(soc),getMinValueInParameter(soc));
-		printSimpleMovingAverageForParams("Temperature",getSimpleMovingAverageOfLastFiveParamValues(temperature));
-		printSimpleMovingAverageForParams("SOC",getSimpleMovingAverageOfLastFiveParamValues(soc));
-		}
-		catch(Exception e) {
-			System.err.println(e);
-		}
 	}
 	
+	/**
+	 * Process the sender input data read from console 
+	 * @param readData - data from console
+	 * @return a record of param data from console i.e. temperature & soc
+	 */
 	public static int[] dataParser(String readData) {
 		if(readData.contains("null"))
 			return null;
@@ -55,18 +75,39 @@ public class ReceiveData {
 		return singleRecord;
 	}
 	
+	/**
+	 * Finds the maximum value in the param list 
+	 * @param paramList - list of a parameter readings
+	 * @return maximum value
+	 */
 	public static int getMaxValueInParameter(List<Integer>paramList) {
 		return Collections.max(paramList);		
 	}
 
+	/**
+	 * Finds the minimum value in the param list
+	 * @param paramList - list of a parameter readings 
+	 * @return minimum value
+	 */
 	public static int getMinValueInParameter(List<Integer>paramList) {
 		return Collections.min(paramList);		
 	}
 	
+	/**
+	 * Prints maximum and minimum value to the console
+	 * @param paramName - paramter name i.e. temperature or soc 
+	 * @param maxValue - maximum value in the given param list
+	 * @param minValue - minimum value in the given param list
+	 */
 	public static void printMaxAndMinValuesInParameter(String paramName, int maxValue, int minValue) {
 		System.out.println(paramName + " Maximum Value : "+ maxValue +"\t"+ "Minimum Value : "+ minValue);
 	}
 	
+	/**
+	 * Calculates the average of last five values in the given param list 
+	 * @param paramList - list of a parameter readings
+	 * @return average of last five values
+	 */
 	public static int getSimpleMovingAverageOfLastFiveParamValues(List<Integer> paramList) {
 		List<Integer> lastFiveValues = paramList.subList(paramList.size() - 5, paramList.size());
 		int sum = 0;
@@ -81,6 +122,11 @@ public class ReceiveData {
 		return sum/5;
 	}
 	
+	/**
+	 * Prints the average to the console
+	 * @param paramName - paramter name i.e. temperature or soc 
+	 * @param averageValue - average of last five value in the param list
+	 */
 	public static void printSimpleMovingAverageForParams(String paramName, int averageValue) {
 		System.out.println("Simple moving average of "+ paramName + " is " + averageValue);
 	}
